56ef4285372e1d9e41f2eb1318d954a35c687797
Naman <naman.6.1998@gmail.com>
2021-06-23 15:55:14 +0530

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const hbs = require('hbs')
 const chalk = require('chalk')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectory = path.join(__dirname, '../public')
@@ -92,6 +93,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
